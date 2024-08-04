e6af5f89712c27f7aafc31200553705f49613b50
lsioctl <laurent.steff@gmail.com>
2024-08-04 12:35:47 -0400

    first commit

---
+++ first/Game1.cs
@@ -0,0 +1,148 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace first;
+
+public class Game1 : Game
+{
+    Texture2D ballTexture;
+    Vector2 ballPosition;
+    float ballSpeed;
+
+    private GraphicsDeviceManager _graphics;
+    private SpriteBatch _spriteBatch;
+    int deadZone;
+
+    public Game1()
+    {
+        _graphics = new GraphicsDeviceManager(this);
+        Content.RootDirectory = "Content";
+        IsMouseVisible = true;
+    }
+
+    protected override void Initialize()
+    {
+        // center of the screen
+        ballPosition = new Vector2(
+            _graphics.PreferredBackBufferWidth / 2,
+            _graphics.PreferredBackBufferHeight / 2
+        );
+        ballSpeed = 300f;
+        deadZone = 4096;
+
+        base.Initialize();
+    }
+
+    protected override void LoadContent()
+    {
+        // Create a new SpriteBatch, which can be used to draw textures.
+        _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+        // TODO: use this.Content to load your game content here
+        ballTexture = Content.Load<Texture2D>("ball");
+    }
+
+    protected override void Update(GameTime gameTime)
+    {
+        if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+            Exit();
+
+        var kstate = Keyboard.GetState();
+
+        // using delta time
+        float updatedBallSpeed = ballSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
+
+        // jeyboard input
+        // TODO: function
+        if (kstate.IsKeyDown(Keys.Up))
+        {
+            ballPosition.Y -= updatedBallSpeed;
+        }
+
+        if (kstate.IsKeyDown(Keys.Down))
+        {
+            ballPosition.Y += updatedBallSpeed;
+        }
+
+        if (kstate.IsKeyDown(Keys.Left))
+        {
+            ballPosition.X -= updatedBallSpeed;
+        }
+
+        if (kstate.IsKeyDown(Keys.Right))
+        {
+            ballPosition.X += updatedBallSpeed;
+        }
+
+        // joystick input
+        // TODO: function
+        if (Joystick.LastConnectedIndex == 0)
+        {
+            // TODO: PlayerIndex.One is a enum, not an int
+            // JoystickState jstate = Joystick.GetState(PlayerIndex.One);
+            JoystickState jstate = Joystick.GetState((int)PlayerIndex.One);
+
+            if (jstate.Axes[1] < -deadZone)
+            {
+                ballPosition.Y -= updatedBallSpeed;
+            }
+            else if (jstate.Axes[1] > deadZone)
+            {
+                ballPosition.Y += updatedBallSpeed;
+            }
+
+            if (jstate.Axes[0] < -deadZone)
+            {
+                ballPosition.X -= updatedBallSpeed;
+            }
+            else if (jstate.Axes[0] > deadZone)
+            {
+                ballPosition.X += updatedBallSpeed;
+            }
+        }
+
+        // Boundary check
+        if (ballPosition.X > _graphics.PreferredBackBufferWidth - ballTexture.Width / 2)
+        {
+            ballPosition.X = _graphics.PreferredBackBufferWidth - ballTexture.Width / 2;
+        }
+        else if (ballPosition.X < ballTexture.Width / 2)
+        {
+            ballPosition.X = ballTexture.Width / 2;
+        }
+
+        if (ballPosition.Y > _graphics.PreferredBackBufferHeight - ballTexture.Height / 2)
+        {
+            ballPosition.Y = _graphics.PreferredBackBufferHeight - ballTexture.Height / 2;
+        }
+        else if (ballPosition.Y < ballTexture.Height / 2)
+        {
+            ballPosition.Y = ballTexture.Height / 2;
+        }
+
+        base.Update(gameTime);
+    }
+
+    protected override void Draw(GameTime gameTime)
+    {
+        GraphicsDevice.Clear(Color.CornflowerBlue);
+
+        _spriteBatch.Begin();
+        _spriteBatch.Draw(
+            ballTexture,
+            ballPosition,
+            null,
+            Color.White,
+            0f,
+            // ensure the ball is really centered and not the top left of the image
+            new Vector2(ballTexture.Width / 2, ballTexture.Height / 2),
+            Vector2.One,
+            SpriteEffects.None,
+            01f
+        );
+        _spriteBatch.End();
+
+        base.Draw(gameTime);
+    }
+}
---
+++ first/Program.cs
@@ -0,0 +1,3 @@
+﻿
+using var game = new first.Game1();
+game.Run();
